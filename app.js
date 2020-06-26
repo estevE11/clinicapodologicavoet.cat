d403410600179cbadc11664f77e7518df462ea76
Roger Esteve <Coconaut7@gmail.com>
2020-06-26 23:43:33 +0200

    Clean version for show

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const app = express();
 const path = require("path");
 const url = require("url");
+var fs = require('fs');
+
+const express = require("express");
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,6 +15,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, function() {
     console.log("Listening to " + PORT);
+});
+
+app.get("/laclinica", function(req, res) {
+    let p = path.join(req.url, "index");
+
+    const files = fs.readdirSync(__dirname + '/public/img/laclinica');
+
+    let args = {
+        url_args: url.parse(req.url, true),
+        files: files,
+        caca: "cacota"
+    }
+    res.render(p.substring(1, p.length), args);
 });
 
 app.get("*", function(req, res) {
